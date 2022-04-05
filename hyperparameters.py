cac8283ea38c7d2661c79fca6c668cb4f8ae1faa
chokladmousse <98423540+chokladmousse@users.noreply.github.com>
2022-04-04 22:24:18 -0700

    Update hyperparameters.py

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,30 +1,23 @@
 __config__ = {
     'data_provider': 'tas500v11.dataloader',
     'network': {
-        'model': 'hourglass',
-        'base': 'resUnet',
-        'prenet': 'same',
+        'model': 'iterative',       # net, hourglass, iterative
+        'base': 'resUnet',          # resnet, resUnet
+        'prenet': 'x8',             # x1, x2, x4, x8
     },
     'inference': {
-        'M': 3,
-        'f': 8,
-        'increase_ratio': 1.5,
-        'normalization': 'layer', # batch or layer
+        'M': 4,
+        'f': 32,
+        'n': 1,
+        'increase_ratio': 2,
+        'normalization': 'layerHW', # batch, layerHW, layerCHW
         'num_class': 10,
-        'scale': 1, # scale of input in comparison to output, should be set by prenet
         'inp_dim': (384, 768),
-        'oup_dim': (384, 768),
-        'max_stack': 4,
+        'max_stack': 8,
     },
     'train': {
-        'train_iters': 1,
-        'valid_iters': 1,
-        'test_iters': 1,
-        'batchsize': 2,
-        'input_res': (256,256),
-        'output_res': (256,256),
+        'batchsize': 8,
         'learning_rate': 1e-3,
-        'momentum': 0.9,
         'weight_decay': 1e-4,
         'loss': 'DiceLoss',
     },
