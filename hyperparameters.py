374002a5ae713e6567a86e0568e93f0cd6639eab
chokladmousse <98423540+chokladmousse@users.noreply.github.com>
2022-05-15 10:17:39 -0700

    Update hyperparameters.py

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,22 +1,22 @@
 __config__ = {
     'data_provider': 'tas500v11.dataloader',
     'network': {
-        'model': 'iterative',       # net, hourglass, iterative
+        'model': 'iterativeV6',       # net, hourglass, iterative
         'base': 'resUnet',          # resnet, resUnet
-        'prenet': 'x8',             # x1, x2, x4, x8
+        'prenet': 'x2',             # x1, x2, x4, x8
     },
     'inference': {
         'M': 4,
-        'f': 32,
+        'f': 60,
         'n': 1,
-        'increase_ratio': 2,
+        'increase_ratio': 1.5,
         'normalization': 'layerHW', # batch, layerHW, layerCHW
         'num_class': 10,
         'inp_dim': (384, 768),
-        'max_stack': 8,
+        'max_stack': 3,
     },
     'train': {
-        'batchsize': 8,
+        'batchsize': 6,
         'learning_rate': 1e-3,
         'weight_decay': 1e-4,
         'loss': 'DiceLoss',
